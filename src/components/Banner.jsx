588141d42079bc7006ac549db9dae3d5459fe9c3
TrevorMadeSomething <54298052+TrevorMadeSomething@users.noreply.github.com>
2021-09-16 16:34:12 -0400

    Update Banner.jsx
    removed dark mode

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import "../styles/banner.scss"
+import React from "react";
+import "../styles/banner.scss";
 
 export default function Banner() {
-    return (
-        <div className="banner">
-            <h1>devfinder</h1>
-            <div className="themeSwapper">
+  return (
+    <div className="banner">
+      <h1>devfinder</h1>
+      {/* <div className="themeSwapper">
                 <h4>DARK</h4>
                 <img src="/assets/icon-moon.svg" alt="moon icon" />
-            </div>
-        </div>
-    )
+            </div> */}
+    </div>
+  );
 }
